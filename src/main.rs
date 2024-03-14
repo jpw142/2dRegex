c1210db797dd67add490291bafa42ca9fa83d67e
Jack Weber <jackweber142@gmail.com>
2024-03-14 00:04:42 -0400

    removed the weird ^m's

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,8 @@
 #![allow(non_snake_case)]
 mod tokenizer;
-
 use tokenizer::picture::Picture;
 use tokenizer::fsm::Fsm;
 use tokenizer::picture::Color;
-// use std::fs;
-
 
 fn main() {
     //let picture = Picture::open_pic("test.png");
@@ -15,7 +12,6 @@ fn main() {
     //f.print();
     //print!("{:?}", f.identify(picture.clone()));
     //println!("");
-
     let picture2 = Picture::open_pic("loop_test.png");
     let picture3 = Picture::open_pic("loop_test2.png");
     let mut fsm = Fsm::builder(picture2.clone());
